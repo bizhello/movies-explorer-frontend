a67edb88efb0975ce401c787ff33b98e7d4b0216
Andrey Bizhov <bijev14@yandex.ru>
2022-10-16 08:46:21 +0300

    d2

--- src/components/Main/Portfolio.js
+++ src/components/Main/Portfolio.js
@@ -4,20 +4,22 @@ function Portfolio() {
     return(
         <section className="portfolio">
             <p className="portfolio__title">Портфолио</p>
-            <div className="portfolio__item">
-                <p className="portfolio__name">Статичный сайт</p>
-                <a className="portfolio__link" href="https://bizhello.github.io/how-to-learn/">.</a>
-            </div>
-            <div className="line"></div>
-            <div className="portfolio__item">
-                <p className="portfolio__name">Адаптивный сайт</p>
-                <a className="portfolio__link" href="https://bizhello.github.io/russian-travel/">.</a>
-            </div>
-            <div className="line"></div>
-            <div className="portfolio__item">
-                <p className="portfolio__name">Одностраничное приложение</p>
-                <a className="portfolio__link" href="https://bizhello.github.io/mesto-react/">.</a>
-            </div>
+            <ul className="portfolio__ul">
+                <li className="portfolio__item">
+                    <a className="portfolio__name" target="_blank"  rel="noreferrer" href="https://bizhello.github.io/how-to-learn/">Статичный сайт</a>
+                    <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/how-to-learn/">.</a>
+                </li>
+                <div className="line"></div>
+                <li className="portfolio__item">
+                    <a className="portfolio__name" target="_blank" rel="noreferrer" href="https://bizhello.github.io/russian-travel/">Адаптивный сайт</a>
+                    <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/russian-travel/">.</a>
+                </li>
+                <div className="line"></div>
+                <li className="portfolio__item">
+                    <a className="portfolio__name" target="_blank" rel="noreferrer" href="https://bizhello.github.io/mesto-react/">Одностраничное приложение</a>
+                    <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/mesto-react/">.</a>
+                </li>
+            </ul>
         </section>
     )
 }
