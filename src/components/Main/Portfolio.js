73634332c8789061d117bdfdaf84d1a0e5fc1c4b
Andrey Bizhov <bijev14@yandex.ru>
2022-10-17 10:12:04 +0300

    d3

--- src/components/Main/Portfolio.js
+++ src/components/Main/Portfolio.js
@@ -9,12 +9,12 @@ function Portfolio() {
                     <a className="portfolio__name" target="_blank"  rel="noreferrer" href="https://bizhello.github.io/how-to-learn/">Статичный сайт</a>
                     <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/how-to-learn/">.</a>
                 </li>
-                <div className="line"></div>
+                <li className="line"></li>
                 <li className="portfolio__item">
                     <a className="portfolio__name" target="_blank" rel="noreferrer" href="https://bizhello.github.io/russian-travel/">Адаптивный сайт</a>
                     <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/russian-travel/">.</a>
                 </li>
-                <div className="line"></div>
+                <li className="line"></li>
                 <li className="portfolio__item">
                     <a className="portfolio__name" target="_blank" rel="noreferrer" href="https://bizhello.github.io/mesto-react/">Одностраничное приложение</a>
                     <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://bizhello.github.io/mesto-react/">.</a>
